cef14ebe936aeb578b8cd030e2ed10e8944c5b14
a_feraci <alexaferaci@gmail.com>
2022-05-11 17:19:37 -0400

    add dack message components and format

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const messageComponents = {
-    verbs: [],
-    adjs: [],
-    nouns: []
+    verbs: [
+        'aggregate', 'architect', 'benchmark', 'brand', 'cultivate', 'deliver', 'deploy', 'disintermediate', 'disrupt', 'drive', 'e-enable',
+        'embrace', 'empower', 'enable', 'engage', 'engineer', 'enhance', 'envisioneer', 'evolve', 'expedite', 'exploit', 'exten', 'facilitate', 
+        'generate', 'grow', 'harness', 'implement', 'incentivize', 'incubate', 'innovate', 'integrate', 'iterate', 'leverage', 'matrix', 'maximize', 
+        'mesh', 'monetize', 'morph', 'optimize', 'orchestrate', 'productize', 'recontextualize', 'redefine', 'reintermediate', 'reinvent', 'repurpose',
+        'revolutionize', 'scale', 'seize', 'strategiz', 'streamline', 'syndicate', 'synergize', 'synthesize', 'target', 'transform', 'transition', 
+        'unleash', 'utilize',' visualize', 'whiteboard'
+    ],
+    adjs: [
+        'B2B', 'B2C', 'back-end', 'best-of-breed', 'bleeding-edge', 'bricks-and-clicks', 'clicks-and-mortar','collaborative', compelling, cross-platform, cross-media, customized,
+        'cutting-edge', 'distributed', 'dot-com', 'dynamic', 'e-business', 'efficient', 'end-to-end', 'nterprise', 'extensible', frictionless, front-end,
+        'global', 'granular', 'holistic', 'impactful', 'innovative', 'integrated', 'interactive', 'intuitive', 'killer', 'leading-edge', magnetic,
+        'mission-critical', 'next-generation', 'one-to-one', 'open-source', 'out-of-the-box', 'plug-and-play', 'proactive', real-time, revolutionary, rich, robust, scalable,
+        'seamless', 'sex', 'sticky', 'strategic', 'synergistic', 'transparent', 'urn-key', 'ubiquitous',' user-centric', 'value-added', vertical,
+        'viral', 'virtual', 'visionar', 'web-enabled', 'wireless', 'world-class' 
+    ],
+    nouns: [
+        'action-items', 'applications', 'architectures', 'bandwidth', 'channels', 'communities', 'content', 'convergence', 
+    'deliverables', 'e-business', 'e-commerce', 'e-markets', 'e-services', 'e-tailers', 'experiences', 'eyeball', 'functionalities', 
+    'infomediaries', 'infrastructures', 'initiatives', 'interfaces', 'markets', 'methodologies', 'metric', 'mindshare', 'models', 
+    'networks', 'niches', 'paradigms', 'partnerships', 'platforms', 'portals', 'relationships', 'ROI', 'synergies', 'web-readiness', 
+    'schemas', 'solutions', 'supply-chains', 'ystems', 'technologies', 'users', 'vortals', 'web services'
+]
 }
